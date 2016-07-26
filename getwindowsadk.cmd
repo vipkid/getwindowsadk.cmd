caaccb6c79c8914555ffaca178b00c8cbff151a9
lea2000 <lea2000@users.noreply.github.com>
2016-07-26 10:39:00 +0200

    Update getwindowsadk.cmd

--- getwindowsadk.cmd
+++ getwindowsadk.cmd
@@ -1,18 +1,17 @@
 REM getwindowsadk.cmd
 REM Copyright (C) 2016 lea2000
 REM 
-REM This program is free software: you can redistribute it and/or modify
-REM it under the terms of the GNU General Public License as published by
-REM the Free Software Foundation, either version 3 of the License, or
-REM (at your option) any later version.
+REM This program is free software: you can redistribute it and/or modify  
+REM it under the terms of the GNU General Public License as published by  
+REM the Free Software Foundation, version 3.
 REM 
-REM This program is distributed in the hope that it will be useful,
-REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM GNU General Public License for more details.
+REM This program is distributed in the hope that it will be useful, but 
+REM WITHOUT ANY WARRANTY; without even the implied warranty of 
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+REM General Public License for more details.
 REM 
-REM You should have received a copy of the GNU General Public License
-REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+REM You should have received a copy of the GNU General Public License 
+REM along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 @echo off
 cls
